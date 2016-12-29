b6b4b21a80ed938ff603aee3929e755b5bdd453d
Carlos Estifen Almario Galindez <carloselmax95@hotmail.com>
2016-12-29 09:49:59 -0500

    clase conexion que contiene las url para conectar al api rest

---
+++ app/src/main/java/com/example/carlos/notas/Conexion.java
@@ -0,0 +1,13 @@
+package com.example.carlos.notas;
+
+/**
+ * Created by Carlos on 29/12/2016.
+ */
+
+public class Conexion {
+
+    public static String URL="http://192.168.0.105:8000/Sistema-Gestion-de-Notas/public/android/";
+    public static String INICIAR_SESION=URL+"iniciarSesion";
+    public static String URL_LISTAR_NOTAS=URL+"traerNotas";
+
+}
